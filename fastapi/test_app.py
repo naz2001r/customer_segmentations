4044a8f28d259df2fe95be3d3b45709be94a8df1
naz2001r <naz2001r@gmail.com>
2022-12-11 11:11:20 +0200

    Added basic tests

---
+++ fastapi/test_app.py
@@ -0,0 +1,18 @@
+from fastapi.testclient import TestClient
+
+from .app import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/scale_methods")
+    assert response.status_code == 200
+
+def test_read_main():
+    response = client.get("/dimension_reduction_methods")
+    assert response.status_code == 200
+
+def test_read_main():
+    response = client.get("/cluster_models")
+    assert response.status_code == 200   
