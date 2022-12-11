80de7627c3043760e54e6b5ca791e1f1150fd0a5
naz2001r <naz2001r@gmail.com>
2022-12-11 12:06:28 +0200

    one more experiment

--- fastapi/test_app.py
+++ fastapi/test_app.py
@@ -1,18 +1,17 @@
 from fastapi.testclient import TestClient
-
 from .app import app
 
 client = TestClient(app)
 
 
-def test_read_main():
+def test_scale_methods():
     response = client.get("/scale_methods")
     assert response.status_code == 200
 
-def test_read_main():
+def test_dimension_reduction_methods():
     response = client.get("/dimension_reduction_methods")
     assert response.status_code == 200
 
-def test_read_main():
+def test_cluster_models():
     response = client.get("/cluster_models")
     assert response.status_code == 200   
